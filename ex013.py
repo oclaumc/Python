737ef885ce64782237ad6ef5a4c2bfecafd7e69e
oclaumc <49166205+oclaumc@users.noreply.github.com>
2021-02-02 13:59:49 -0300

    Create ex013.py

---
+++ ex013.py
@@ -0,0 +1,4 @@
+print('\033[0;30;47mOlá Mundo!\033[m') #Style 0,1,4,7[Sem estilo, negrito, sublinado, nagativo]
+print('\033[1;31;46mOlá Mundo!\033[m') #Text 30,31,32,33,34,35,36,37[branco,vemelho,verde, amaraelo,azul,roxo,turquesa,cinza]
+print('\033[4;32;45mOlá Mundo!\033[m') #background 40,41,42,43,44,45,46,47[branco,vemelho,verde, amaraelo,azul,roxo,turquesa,cinza]
+print('\033[7;30;44mOlá Mundo!\033[m') #Pesquisar colorrider

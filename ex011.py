fe895a492b04af6d6330891844830b388a557633
oclaumc <49166205+oclaumc@users.noreply.github.com>
2021-02-02 13:58:37 -0300

    Create ex011.py

---
+++ ex011.py
@@ -0,0 +1,9 @@
+'''Leia varios nomes e escreve um dos nomes escolhidos de forma aletatoria'''
+import random
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('O nome escolhido foi {}'.format(escolhido))

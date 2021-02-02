502b72565971fd042c0acf3a917d7d926f61ca84
oclaumc <49166205+oclaumc@users.noreply.github.com>
2021-02-02 13:56:23 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,6 @@
+'''leia o comprimento do cateto oposto e do cateto adjacente de um
+triângulo retângulo , calcule e mostre o compimento da hipotenusa.'''
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co ** 2 + ca ** 2) ** (1 / 2)
+print('A hipotenusa vai medir {:.2f}'.format(hi))

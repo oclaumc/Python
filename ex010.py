eeb6113e3f43a028cefd129b3f322d4f8798543b
oclaumc <49166205+oclaumc@users.noreply.github.com>
2021-02-02 13:58:00 -0300

    Create ex010.py

---
+++ ex010.py
@@ -0,0 +1,11 @@
+''' Leia um ângulo e exiba na tela o valor seno cosseno tangente
+desse ângulo'''
+
+import math 
+angulo = float(input('Digite o ângulo que você deseja: '))
+seno = math.sin(math.radians(angulo))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
+cosseno = math.cos(math.radians(angulo))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
+tangente = math.tan(math.radians(angulo))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(angulo, tangente))

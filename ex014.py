265c76418065af36a1f1ea4bb01c3fb94dd5a525
oclaumc <49166205+oclaumc@users.noreply.github.com>
2021-02-02 14:00:20 -0300

    Create ex014.py

---
+++ ex014.py
@@ -0,0 +1,6 @@
+nome =  'Clauberto'
+cores = {'limpa':'\033[m',
+         'azul':'\033[43m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m'}
+print('Olá ! Muito prazer em te conhecer, {}{}{}!!'.format(cores['amarelo'], nome, cores['limpa']))
